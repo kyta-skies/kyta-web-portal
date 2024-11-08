b24a48d1fbdba7a228f798b802022655734a5a59
Salu Alhassan <alhassansalu1@gmail.com>
2024-11-08 08:31:24 +0000

    update

--- components/mainFooter.vue
+++ components/mainFooter.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="flex justify-between items-center w-full max-w-sm">
-        <h1>&copy; Kyta Skies</h1>
+    <div class="flex justify-between items-center w-full max-w-sm pb-4">
+        <h1 class="text-gray-500">Kyta Skies </h1>
+        <div class="text-gray-500">
+            <span>&copy;</span><span>2024</span>
+        </div>
         <div class="flex flex-row gap-2">
-            <IconsWhatapp class="text-black hover:text-green-600 text-4xl " />
-            <IconsSupport class="text-black hover:text-blue-600 text-4xl" />
+            <a href=“tel:+233546383716”><IconsSupport class="text-gray-500 hover:text-blue-600 text-3xl" /></a>
+            <a href=“tel:+233546383716”><IconsWhatapp class="text-gray-500 hover:text-green-600 text-3xl " /></a> 
         </div>
     </div>
 </template>
--- pages/resetPassword.vue
+++ pages/resetPassword.vue
@@ -1,4 +1,10 @@
 <template>
-    <P>Reset password
-    </P>
+    <div class="flex flex-row h-screen justify-center items-center gap-2">
+        <a href=“tel:+233546383716” class="w-xl p-6 bg-slate-400 rounded border-b-4 border-[#980E70] hover:w-2xl">
+            <IconsSupport class="text-gray-500 hover:text-blue-600 text-[50px] " />
+        </a>
+        <a href=“#” class="w-xl p-6 bg-slate-400 rounded border-b-4 border-[#980E70] hover:w-2xl">
+            <IconsWhatapp class="text-gray-500 hover:text-green-400 text-[50px] " />
+        </a>
+    </div>
 </template>
